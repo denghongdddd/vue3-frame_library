f22149dd6fd731520b6b03d7576c1daa5138efcd
denghong <m13724205240@163.com>
2021-10-03 15:12:22 +0800

    reflresh

--- Dpackages/config/customObj.js
+++ Dpackages/config/customObj.js
@@ -1,14 +1,4 @@
 +function(){
-  setTimeout(()=>{
-    Object.prototype.dh_value_=function(key,defaultValue=null){
-      if(this.hasOwnProperty(key))return this[key]
-      return defaultValue
-    }
-  })
-}()
-
-+function(){
-  console.log("----------------------asdfasf----")
   Array.prototype.dFor=function(call){
     for(var i in this){
       var res=call(i,this[i],this)
