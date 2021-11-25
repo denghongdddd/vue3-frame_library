643881b59f01f4de5220188575ffac710256c6ca
dingtalk_umvbsp <m13724205240@163.com>
2021-11-25 17:22:47 +0800

    master

--- Dpackages/config/utils.js
+++ Dpackages/config/utils.js
@@ -2,6 +2,77 @@
 export const dataType=function(target,...str){
 	return str.indexOf( Object.prototype.toString.call(target).slice(8,-1).toLocaleLowerCase() )>=0
 }
+/** 随机生成颜色 */
+export class Colors{
+	#cArray=["0","1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+	constructor(hslLength=1){
+		this.hslLength= hslLength;
+	}
+	rgbArray(){
+		var hslArray=this.getHslArray();
+		if(!hslArray.length)return [];
+		var rgbArray = hslArray.map(v=>this.hslToRgb.apply(this, v))
+		return rgbArray.map(v=>({
+			//三原色值
+			value:v,
+			//16进制的颜色
+			hex: this.rgbToHexadecimal(v),
+			//rgb颜色
+			rgb: v.toString()
+		}))
+	}
+	rgbToHexadecimal(rgb){
+		if(rgb&&rgb.length==3){
+			return '#'
+			+`${this.#cArray[Math.floor(rgb[0]/16)]}${this.#cArray[rgb[0]%16]}`
+			+`${this.#cArray[Math.floor(rgb[1]/16)]}${this.#cArray[rgb[1]%16]}`
+			+`${this.#cArray[Math.floor(rgb[2]/16)]}${this.#cArray[rgb[2]%16]}`
+		}else return ''
+	}
+	hslToRgb(H, S, L){
+		var R, G, B;
+		if(+S === 0){
+			R=G=B=L;//饱和度为0 为灰色
+		}else{
+			var hue2Rgb = function(p, q, t) {
+				if(t < 0) t += 1;
+				if(t > 1) t -= 1;
+				if(t < 1 / 6) return p + (q - p) * 6 * t;
+				if(t < 1 / 2) return q;
+				if(t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+				return p;
+			};
+			var Q = L < 0.5 ? L * (1 + S) : L + S - L * S;
+			var P = 2 * L - Q;
+			R = hue2Rgb(P, Q, H + 1 / 3);
+			G = hue2Rgb(P, Q, H);
+			B = hue2Rgb(P, Q, H - 1 / 3);
+		}
+		return [Math.round(R * 255), Math.round(G * 255), Math.round(B * 255)];
+	}
+	randomHsl(){
+		return [Math.random(),Math.random(),Math.random()]
+	}
+	getHslArray(){
+		var HSL=[];
+		for(var i=0;i<this.hslLength;i++){
+			var ret=this.randomHsl();
+			// 颜色相邻颜色差异须大于 0.25
+			if(i>0 && Math.abs(ret[0] - HSL[i-1][0])<0.25){
+				i--
+				continue;// 重新获取随机色
+			}
+			ret[1] = 0.7 + (ret[1] * 0.2); // [0.7 - 0.9] 排除过灰颜色
+			ret[2] = 0.4 + (ret[2] * 0.3); // [0.4 - 0.8] 排除过亮过暗色
+			// 数据转化到小数点后两位
+			ret = ret.map(function(item) {
+				return parseFloat(item.toFixed(2));
+			});
+			HSL.push(ret);
+		}
+		return HSL;
+	}
+}
 /** 身份证验证 */
 export const IDCode=function (data) {
 	// 返回验证结果，校验码和格式同时正确才算是合法的身份证号码
