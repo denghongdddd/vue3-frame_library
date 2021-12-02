b178bd2b90907bde8d447a759d25938d653fb26c
dingtalk_umvbsp <m13724205240@163.com>
2021-12-02 10:56:31 +0800

    master

--- Dpackages/config/customObj.js
+++ Dpackages/config/customObj.js
@@ -5,4 +5,16 @@
       if(!res)return
     }
   }
+  Object.prototype.dDel=function(){
+    var list=[].concat(...arguments)
+    var keys=Object.keys(this)
+    for(var i of keys){
+      if(!list.some(v=>v==i)){
+        delete this[i]
+      }
+    }
+    return (val={})=>{
+      Object.assign(this,val)
+    }
+  }
 }()
