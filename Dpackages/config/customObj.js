bf4c0d70c4197509decd9574efb02527230efe7d
dingtalk_umvbsp <m13724205240@163.com>
2021-12-02 11:04:55 +0800

    masger

--- Dpackages/config/customObj.js
+++ Dpackages/config/customObj.js
@@ -1,20 +1,22 @@
 +function(){
-  Array.prototype.dFor=function(call){
-    for(var i in this){
-      var res=call(i,this[i],this)
-      if(!res)return
-    }
-  }
-  Object.prototype.dDel=function(){
-    var list=[].concat(...arguments)
-    var keys=Object.keys(this)
-    for(var i of keys){
-      if(!list.some(v=>v==i)){
-        delete this[i]
+  setTimeout(()=>{
+    Array.prototype.dFor=function(call){
+      for(var i in this){
+        var res=call(i,this[i],this)
+        if(!res)return
       }
     }
-    return (val={})=>{
-      Object.assign(this,val)
+    Object.prototype.dDel=function(){
+      var list=[].concat(...arguments)
+      var keys=Object.keys(this)
+      for(var i of keys){
+        if(!list.some(v=>v==i)){
+          delete this[i]
+        }
+      }
+      return (val={})=>{
+        Object.assign(this,val)
+      }
     }
-  }
+  })
 }()
